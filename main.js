f9476c18e358720628093c9917d05ba974bb292b
Murillo Mendonca Mascarenha <mmendoncamascarenha@gmail.com>
2025-02-26 16:52:30 -0300

    ultimas atualizacoes

--- main.js
+++ main.js
@@ -1,39 +1,55 @@
-
 console.log("Processo principal")
-const { app, BrowserWindow, nativeTheme, Menu} = require('electron')
 
-// Janela Principal
+const { app, BrowserWindow, nativeTheme, Menu, ipcMain } = require('electron')
+
+// Esta linha esta relacionada ao preload.js
+const path = require('node:path')
+
+// Janela principal
 let win
 const createWindow = () => {
-    // a linha abaixo define o tema(claro ou escuro)
-    nativeTheme.themeSource = 'dark' // (dark ou light)
-   win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        // autoHideMenuBar: true,
-        // minimizable: false,
-        resizable: false
+  // a linha abaixo define o tema (claro ou escuro)
+  nativeTheme.themeSource = 'dark' //(dark ou light)
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // autoHideMenuBar: true,
+    // minimizable: false,
+    resizable: false,
+    // ativação do preload.js
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   // menu personalizado
   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-
   win.loadFile('./src/views/index.html')
+
+  // recebimento dos pedidos do renderizador para abertura de janelas (botoes)
+  // autorizado no preload.js
+  ipcMain.on('client-window', () => {
+    clientWindow()
+  })
+
+  ipcMain.on('os-window', () => {
+    osWindow()
+  })
 }
 
-//  Janeçla sobre
-function aboutWindow(){
+// Janela sobre 
+function aboutwindows() {
   nativeTheme.themeSource = 'light'
-// A linha abaixo obtém a janela principal
+  // a linha abaixo obtem a janela principal
   const main = BrowserWindow.getFocusedWindow()
-  let about 
-// Estabelecer uma relação hieráquica entre janelas
+  let about
+  // Estabelece uma relação hierárquica entre janelas
   if (main) {
-    // Criar tabela sobre
+    // Criar a janela sobre
     about = new BrowserWindow({
       width: 360,
-      height: 220,
+      height: 200,
       autoHideMenuBar: true,
       resizable: false,
       minimizable: false,
@@ -41,62 +57,123 @@ function aboutWindow(){
       modal: true
     })
   }
+
   // Carregar o documento html na janela
   about.loadFile('./src/views/sobre.html')
 }
 
-// Iniciar A APLICAÇÃO
-app.whenReady().then(() => {
-    createWindow()
+// janela clientes
+let client
+function clientWindow() {
+  nativeTheme.themeSource = 'light'
+  const main = BrowserWindow.getFocusedWindow()
+  if (main) {
+    client = new BrowserWindow({
+      width: 1010,
+      height: 720,
+      //autoHideMenuBar: true,
+      resizable: false,
+      parent: main,
+      modal: true
+    })
+  }
+    client.loadFile('./src/views/cliente.html')
+    client.center()
   
+}
+
+// janela OS
+let os
+function osWindow() {
+  nativeTheme.themeSource = 'light'
+  const main = BrowserWindow.getFocusedWindow()
+  if (main) {
+    os = new BrowserWindow({
+      width: 1010,
+      height: 720,
+      //autoHideMenuBar: true,
+      resizable: false,
+      parent: main,
+      modal: true
+    })
+  }
+  os.loadFile('./src/views/os.html')
+  os.center()
+}
+
+// Iniciar a aplicação 
+app.whenReady().then(() => {
+  createWindow()
+
+  app.whenReady().then(() => {
+    createWindow()
+
     app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
       }
     })
   })
-  
+
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
       app.quit()
     }
   })
-  //  Reduzir logs não criticos
-  app.commandLine.appendSwitch('log-level', '3')
-  //  Template do menu
-  const template = [
-    {
-      label: 'Cadastro',
-      submenu: [
-        {
-          label: 'Clientes'
-        },
-        {
-          label: 'OS'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Sair', 
-          click: () => app.quit(),
-          accelerator: 'Alt+F4'
-        }
-      ]
-    },
-    {
-      label: 'Relatórios'
-    },
-    {
-     label: 'Ferramentas',
-     submenu: [
+})
+
+// Reduzir logs não criticos
+app.commandLine.appendSwitch('log-level', '3')
+
+// Template do menu
+const template = [
+  {
+    label: 'Cadastro',
+    submenu: [
       {
-        label: 'aplicar zoom',
+        label: 'Clientes',
+        click: () => clientWindow()
+      },
+      {
+        label: 'OS',
+        click: () => osWindow()
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Sair',
+        click: () => app.quit(),
+        accelerator: 'Alt+F4'
+      }
+    ]
+  },
+
+  {
+    label: 'Relatórios',
+    submenu: [
+      {
+        label: "Clientes"
+      },
+      {
+        label: "OS abertas"
+      },
+      {
+        label: "OS concluídas"
+      }
+    ]
+  },
+
+  {
+    label: 'Ferramentas',
+    submenu: [
+      {
+        label: 'Aplicar zoom',
         role: 'zoomIn'
       },
       {
         label: 'Reduzir',
-        role: 'zoomOut'
+        role: 'zoomOut',
       },
       {
         label: 'Restaurar o zoom padrão',
@@ -107,21 +184,21 @@ app.whenReady().then(() => {
       },
       {
         label: 'Recarregar',
-        role:  'reload'
+        role: 'reload'
       },
       {
-        label: 'Ferramentas do Desenvolvedor',
+        label: 'Ferramentas do desenvolvedor',
         role: 'toggleDevTools'
       }
-     ]
-    },
-    {
-      label: 'Ajuda',
-      submenu: [
-        {
-          label: 'Sobre',
-          click: () => aboutWindow()
-        }
-      ]
-    }
-  ] 
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'Sobre',
+        click: () => aboutwindows()
+      }
+    ]
+  }
+]
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -3,42 +3,42 @@
  * @author Murillo Mendonca 
  */
 
-// Função para formatar a data
+//***************************************************Função para formatar a data****************************************************
 function formatarData() {
     const hoje = new Date();
     const opcoes = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' };
     let dataFormatada = hoje.toLocaleDateString('pt-BR', opcoes);
 
-    // Capitaliza a primeira letra do dia da semana
+    //***************************************Capitaliza a primeira letra do dia da semana******************************************
     dataFormatada = dataFormatada.replace(/^\w/, (c) => c.toUpperCase());
     return dataFormatada;
 }
 
-// Objeto para armazenar os dados da data
+//***********************************************Objeto para armazenar os dados da data*******************************************
 let dados = {
     anoAtual: new Date().getFullYear(),
     dataAtual: formatarData(),
 };
 
-// Exibindo o ano no HTML (no rodapé)
+//***************************************************Exibindo o ano no HTML (no rodapé)********************************************
 let ano = document.getElementById('copyrightYear');
 if (ano) {
     ano.innerHTML = dados.anoAtual;
 }
 
-// Exibindo a data no HTML (no rodapé)
+//***************************************************Exibindo a data no HTML (no rodapé)********************************************
 let dataElemento = document.getElementById('dataAtual');
 if (dataElemento) {
     dataElemento.innerHTML = dados.dataAtual;
 }
 
-// Função para atualizar a data, caso necessário
+//***********************************************Função para atualizar a data, caso necessário************************************
 function atualizarData() {
-    dados.dataAtual = formatarData(); // Atualiza a data no objeto
+    dados.dataAtual = formatarData(); //*Atualiza a data no objeto*
     if (dataElemento) {
-        dataElemento.innerHTML = dados.dataAtual; // Atualiza o conteúdo no HTML
+        dataElemento.innerHTML = dados.dataAtual; //*Atualiza o conteúdo no HTML*
     }
 }
 
-// Chamada para atualizar a data automaticamente, caso necessário
-setInterval(atualizarData, 1000 * 60 * 60); // Atualiza a data a cada hora
+//******************************************Chamada para atualizar a data automaticamente, caso necessário**************************
+setInterval(atualizarData, 1000 * 60 * 60); //*Atualiza a data a cada hora*
